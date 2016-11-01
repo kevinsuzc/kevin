ae009edf9f51f13501755bd1418320f4075d2419
zichaosu <szc769104@gmail.com>
2016-11-02 00:49:14 +1100

    CV modify

--- gulp/build.js
+++ gulp/build.js
@@ -94,4 +94,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/'),path.join(conf.paths.publish, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['clean','html', 'fonts', 'other']);
