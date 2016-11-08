b2f5864af66aeb9e7c33fb84b118147d27499657
zichaosu <szc769104@gmail.com>
2016-11-08 20:55:07 +1100

    fix nav bar bug

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,36 +6,12 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($window,$timeout, webDevTec, toastr,$state) {
+  function MainController($scope,$state) {
     var vm = this;
 
     $state.go('index.home');
-  //   vm.awesomeThings = [];
-  //   vm.classAnimation = '';
-  //   vm.creationDate = 1476596428619;
-  //   vm.showToastr = showToastr;
-  //
-  //   activate();
-  //
-  //   function activate() {
-  //     getWebDevTec();
-  //     $timeout(function() {
-  //       vm.classAnimation = 'rubberBand';
-  //     }, 4000);
-  //   }
-  //
-  //   function showToastr() {
-  //     toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-  //     vm.classAnimation = '';
-  //   }
-  //
-  //   function getWebDevTec() {
-  //     vm.awesomeThings = webDevTec.getTec();
-  //
-  //     angular.forEach(vm.awesomeThings, function(awesomeThing) {
-  //       awesomeThing.rank = Math.random();
-  //     });
-  //   }
-
+    $scope.isNavCollapsed = true;
+    $scope.isCollapsed = false;
+    $scope.isCollapsedHorizontal = false;
   }
 })();
