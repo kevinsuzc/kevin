372d23e80bc520ae5d7c33f18c5f9a5e64e9b07d
kevinsu <szc769104@gmail.com>
2017-03-03 16:39:12 +1100

    update

--- src/app/tpl/calendar.controller.js
+++ src/app/tpl/calendar.controller.js
@@ -9,6 +9,30 @@ angular.module('kevin')
     var vm = this;
     console.log("calendar");
 
+    // function generateTable(thisMonthFirstDate) {
+    //   for (var i = 0; i < 6; i++){
+    //     var newRow = document.createElement("tr");
+    //     for(var j = 0; j < 7; j++){
+    //       var newDate = document.createElement("td");
+    //       var date;//获取日期信息
+    //       newDate.innerText = thisMonthFirstDate;
+    //       newRow.appendChild(newDate);
+    //     }
+    //     dateTable.appendChild(newRow);
+    //   }
+    // }
+    //
+    // var currentDate = new Date();
+    //
+    // var month = currentDate.getMonth();
+    // console.log(month);
+    // var year = currentDate.getFullYear();
+    // var thisMonthDay = new Date(year, month, 1);
+    // var thisMonthFirstDay = thisMonthDay.getDay();
+    // var thisMonthFirstDate = new Date(year, month, - thisMonthFirstDay);
+    // console.log(thisMonthDay,thisMonthFirstDay,thisMonthFirstDate);
+    // generateTable(thisMonthFirstDate);  //生成日历主体的日期区域
+
     vm.click = function (day, size) {
 
       var modalInstance = $uibModal.open({
